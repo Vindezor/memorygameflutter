8db04e893708e7ff4356cb61902b94329ef7f2e7
Richard <richarddavidrv13@gmail.com>
2022-03-19 18:41:18 -0400

    added willpopscope and score

--- lib/app/ui/game/widgets/lose_game/lose_game.dart
+++ lib/app/ui/game/widgets/lose_game/lose_game.dart
@@ -11,48 +11,63 @@ class LoseGamePage extends StatefulWidget {
 class _LoseGamePageState extends State<LoseGamePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          const Center(
-            child: Text(
-              "You lose",
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
+    final score = (ModalRoute.of(context)!.settings.arguments as Map)['score'];
+    return WillPopScope(
+      onWillPop: () async {
+        return false;
+      },
+      child: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Center(
+              child: Text(
+                "You lose",
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              TextButton(
-                child: const Text(
-                  "Retry",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
+            Center(
+              child: Text(
+                "Score: $score",
+                style: const TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
                 ),
-                onPressed: () => Navigator.of(context).pushNamed(Routes.GAME),
               ),
-              TextButton(
-                child: const Text(
-                  "Home Page",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TextButton(
+                  child: const Text(
+                    "Retry",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
                   ),
+                  onPressed: () => Navigator.of(context).pushReplacementNamed(Routes.GAME),
                 ),
-                onPressed: () => Navigator.pop(context),
-              ),
-            ],
-          )
-        ],
+                TextButton(
+                  child: const Text(
+                    "Home Page",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  onPressed: () => Navigator.pop(context),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
