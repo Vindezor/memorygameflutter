c1e6d7d95fcf4edc63c3f6e4bdefe2df0a5dec37
Richard <richarddavidrv13@gmail.com>
2022-03-19 18:40:00 -0400

    added win page

--- lib/app/routes/pages.dart
+++ lib/app/routes/pages.dart
@@ -3,6 +3,7 @@ import 'package:memorygame/app/routes/routes.dart';
 import 'package:memorygame/app/ui/about/about_page.dart';
 import 'package:memorygame/app/ui/game/game_page.dart';
 import 'package:memorygame/app/ui/game/widgets/lose_game/lose_game.dart';
+import 'package:memorygame/app/ui/game/widgets/win_game/win_game.dart';
 import 'package:memorygame/app/ui/home/home_page.dart';
 import 'package:memorygame/app/ui/splash/splash_page.dart';
 
@@ -13,5 +14,6 @@ Map<String, Widget Function(BuildContext context)> appRoutes() {
     Routes.ABOUT: (_) => const AboutPage(),
     Routes.GAME: (_) => const GamePage(),
     Routes.LOSE: (_) => const LoseGamePage(),
+    Routes.WIN: (_) => const WinGamePage(),
   };
 }
